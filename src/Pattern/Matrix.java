971d8e891bcd0aae91928ba7d80ded539504793e
Vineet Joshi <jvineet64@gmail.com>
2024-09-28 12:08:51 +0530

    first commit

---
+++ src/Pattern/Matrix.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class Matrix {
+    private final int row;
+    private final int column;
+
+    public Matrix(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    private void generatePattern() {
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < column; j++) {
+                System.out.print("* ");
+            }
+        }
+    }
+}
