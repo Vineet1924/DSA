509f399fc87739ff2a4875a1cb82c20cc330881d
Vineet Joshi <jvineet64@gmail.com>
2024-09-28 12:47:43 +0530

    increasing reverse pyramid

--- src/Pattern/ReverseHalfPyramid.java
+++ src/Pattern/ReverseHalfPyramid.java
@@ -6,12 +6,22 @@ public class ReverseHalfPyramid {
     public ReverseHalfPyramid(int n) {
         this.n = n;
         generatePattern();
+        generateIncreasingNumberPattern();
     }
 
     private void generatePattern() {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n - i; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void generateIncreasingNumberPattern() {
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= n - i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
