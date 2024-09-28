9bb75c18f49d532d8e57014450b4d944313a5569
Vineet Joshi <jvineet64@gmail.com>
2024-09-28 12:28:27 +0530

    increasing number pattern

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import Pattern.Pyramid;
-
 public class Main {
     public static void main(String[] args) {
-        Pyramid pattern = new Pyramid(5);
+
     }
 }
--- src/Pattern/Pyramid.java
+++ src/Pattern/Pyramid.java
@@ -6,12 +6,22 @@ public class Pyramid {
     public Pyramid(int n) {
         this.n = n;
         generatePattern();
+        generateIncreaseNumberPattern();
     }
 
     private void generatePattern() {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j <= i; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void generateIncreaseNumberPattern() {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
