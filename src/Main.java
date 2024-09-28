2efa4c704d29e25af825aab050d5e57b934a8047
Vineet Joshi <jvineet64@gmail.com>
2024-09-28 14:40:50 +0530

    binary triangle pattern

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import Pattern.Daimond;
 import Pattern.HalfDaimond;
+import Pattern.HalfPyramid;
 import Pattern.Pyramid;
 
 public class Main {
     public static void main(String[] args) {
-        HalfDaimond pattern = new HalfDaimond(10);
+        HalfPyramid pattern = new HalfPyramid(5);
     }
 }
--- src/Pattern/HalfPyramid.java
+++ src/Pattern/HalfPyramid.java
@@ -8,6 +8,7 @@ public class HalfPyramid {
         generatePattern();
         generateNumberPattern();
         generateIncreaseNumberPattern();
+        generateBinaryHalfPyramid();
     }
 
     private void generatePattern() {
@@ -32,6 +33,16 @@ public class HalfPyramid {
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void generateBinaryHalfPyramid() {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                if((j - i) % 2 == 0) System.out.print("1 ");
+                else System.out.print("0 ");
             }
             System.out.println();
         }
