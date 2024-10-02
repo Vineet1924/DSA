074975df230c8a48f72069851f440115b4f0972b
Vineet Joshi <jvineet64@gmail.com>
2024-10-02 18:47:55 +0530

    fibonacci

--- src/Recursion/Number.java
+++ src/Recursion/Number.java
@@ -1,6 +1,7 @@
 package Recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Number {
     public void printNumbers(int n) {
@@ -66,6 +67,19 @@ public class Number {
         if(!Character.isLetterOrDigit(s.charAt(j))) return palindromeHelper(s, i, --j);
         if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
         return palindromeHelper(s, ++i, --j);
+    }
+
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return fibo(n, dp);
+    }
+
+    private int fibo(int n, int[] arr) {
+        if(n == 0 || n == 1) return n;
+        if(arr[n] != -1) return arr[n];
+        arr[n] = (fibo(n - 1, arr) + fibo(n - 2, arr));
+        return arr[n];
     }
 }
 
